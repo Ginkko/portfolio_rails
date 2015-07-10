b853135944ceec6d13df84dc2c4e527a215b61da
Alec <Incubusalec@gmail.com>
2015-07-10 21:07:44 +0000

    Add integration and unit testing for comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,13 @@ FactoryGirl.define do
     password_confirmation('123456789')
     admin?(true)
   end
+
+  factory(:post) do
+    name('Test Post')
+    description('Test Description')
+  end
+
+  factory(:comment) do
+    description('Test Comment')
+  end
 end
