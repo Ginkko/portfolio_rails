a92aab9315b4fa28b162ecf23b23d8ce2df34acc
Alec <Incubusalec@gmail.com>
2015-07-10 19:42:26 +0000

    Add comments model, views, and controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,46 @@
+class CommentsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      flash[:alert] = "Comment Added"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:alert] = "Comment Modified"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private def comment_params
+    params.require(:comment).permit!
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   resources "skills"
   resources "projects"
-  resources "posts"
+  resources "posts" do
+    resources "comments", except: ["show", "index"]
+  end
 end
