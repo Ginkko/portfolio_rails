d20db8d475c0f8261a9c49f6849a157281b4014d
Alec <Incubusalec@gmail.com>
2015-07-10 19:54:21 +0000

    Add user show and index. Update Posts and comments views for linking to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "projects#index"
-
+  resources "users", only: ["show", "index"]
   resources "skills"
   resources "projects"
   resources "posts" do
