e5834ffd2c649b0c099209d3d351ee5a1935076b
Alec <Incubusalec@gmail.com>
2015-07-18 18:47:28 +0000

    Add modeling and unit testing for references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710192029) do
+ActiveRecord::Schema.define(version: 20150718183751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20150710192029) do
 
   add_index "projects_skills", ["project_id"], name: "index_projects_skills_on_project_id", using: :btree
   add_index "projects_skills", ["skill_id"], name: "index_projects_skills_on_skill_id", using: :btree
+
+  create_table "references", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string   "name"
